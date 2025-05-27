73c5df5df6a6c95caf252537770a4dd93cbbd78f
Stephanie <adomaa094@gmail.com>
2025-05-27 20:05:35 +0000

    argument added

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,10 @@
-// no argument = "No arguments"
+// no argument = "No argument"
 //only one = print "Argument found"
 //Otherwise = "Arguments found"
+
 let aVar = process.argv.length - 2
 if(aVar == 0){
-    console.log("No arguments")
+    console.log("No argument")
 } else if (aVar == 1){
     console.log("Argument found")
 } else {
