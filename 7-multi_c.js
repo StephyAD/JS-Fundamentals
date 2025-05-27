51d84906ba4dfa015d74ff06fa2dd55d6f8e1cf4
Stephanie <adomaa094@gmail.com>
2025-05-27 21:35:15 +0000

    added for loop file

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+
+
+let x = parseInt (process.argv[2]) 
+if (isNaN(x)){
+    console.log ("Missing number of occurrences")
+} else if (x>0 ){
+    for(i = 0; i< x; i++){
+        console.log("C is fun")
+    }
+}
